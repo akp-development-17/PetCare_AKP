d17670f0b2121a4d4073b72befc5e28080901301
ANISH KUMAR PATEL <104623412+akp-development-17@users.noreply.github.com>
2023-04-04 16:40:21 +0530

    Add files via upload

---
+++ log1s.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Petcare</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/css/s_l.css">
+</head>
+<body>
+<?php
+    header("refresh:2;url=home.php");
+    ?>
+	<div class="background">
+		<div class="overlay">
+			<div class="content">
+				<h1>Login Succesfull 😄</h1>
+                <h5>Redirecting to Home...... </h5>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ reg1s.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Petcare</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/css/s_l.css">
+</head>
+<body>
+<?php
+    header("refresh:2;url=login.php");
+    ?>
+	<div class="background">
+		<div class="overlay">
+			<div class="content">
+				<h1>Registration Succesfull 😄</h1>
+                <h5>Redirecting to Login...... </h5>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
